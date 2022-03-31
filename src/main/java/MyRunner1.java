33f02bed6096a2f4a2556f1ffa4b61907e1eead6
chirag <chiragdhameja.code@gmail.com>
2022-03-31 16:36:05 +0530

    Initial commit

---
+++ src/main/java/MyRunner1.java
@@ -0,0 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyRunner1 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.get("https://github.com/");
+
+        WebElement webElement = webDriver.findElement(By.cssSelector("[name='q']"));
+        String s_serachitem = "Selenium";
+
+        webElement.sendKeys(s_serachitem);
+        webElement.sendKeys(Keys.ENTER);
+
+        webDriver.quit();
+
+    }
+}
